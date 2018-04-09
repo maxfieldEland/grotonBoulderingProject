988abf6c95b05dcaa1929629975d41131651ebfc
maxfieldEland <37519095+maxfieldEland@users.noreply.github.com>
2018-04-09 11:19:14 -0400

    initial upload
    Site map for groton bouldering project, links to maps and areas page

---
+++ projectmap.php
@@ -0,0 +1,56 @@
+
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Groton Boulder Project</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Maxfield Green and Colby Yee">
+        <meta name="description" content="A site map for the groton boulder project">
+        <style type="text/css">
+            figure {
+                border: thin solid darkslategray;
+                border-radius: 0.4em;
+                padding: 0.5em;
+                text-align: center;
+            }
+
+            figcaption {
+                color: #839e99;
+                font-style: italic;
+                text-align: center;
+            }
+
+            img{
+                border-radius: 0.4em;
+                max-width: 100%
+            }
+
+            .right{
+                float: right;
+                margin-left: 1em;
+            }
+
+            .small {
+                width: 150px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <figure class="right small">
+            <img alt="" src="gem.jpg">
+            <figcaption>The Gem Boulder, and boy is it a gem.</figcaption>
+        </figure>
+        <h1 align = "center" >Welcome to the Groton Boulder Project</h1>
+        <p align = "center"> Here you will find an interactive and comprehensive guide to bouldering in Groton State Forest</p>
+
+        <ul align = "center">
+            <li><a href="whygroton.php">Why Groton</a></li>
+            <li><a href ="map.php"> Map of Areas</a></li>
+            <li><a href ="areas.php"> Areas </a></li>
+ 
+        </ul>
+    </body>
+</html>
