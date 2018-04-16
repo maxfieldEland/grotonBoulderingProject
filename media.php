dca502d6963acd273829017234dacd83e041a568
chosslover5 <38412956+chosslover5@users.noreply.github.com>
2018-04-15 22:55:12 -0400

    Update media.php

--- media.php
+++ media.php
@@ -1,4 +1,4 @@
 <!DOCTYPE HTML>
 
-<iframe src="https://player.vimeo.com/video/165481715" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<p><a href="https://vimeo.com/165481715">Bouldering at Groton</a> from <a href="https://vimeo.com/colbyyee">Colby Yee</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+<iframe src="https://player.vimeo.com/video/165481715?color=ff9933" width="1920" height="1080" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+<p></p>
